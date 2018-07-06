ef6aec594109ab31f58e66e846401c7d2ccf7eb9
Michael Borromeo <mborromeo@gmail.com>
2018-07-06 15:31:23 -0400

    Finish copy link

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,16 +8,31 @@ class Header extends Component {
     return (
       <div className="Header">
         <div className="header-text">Objective Personality Helper</div>
-        <div className="header-reset-link">
-          <button className="btn btn-link" onClick={this.props.resetChoices}>
-            Reset
-          </button>
-
-          {this.props.encodedChoiceStates}
-
+        <div className="header-reset-link row">
+          <div className="col-1 offset-2">
+            <button className="btn btn-link" onClick={this.copyLink}>Copy Link</button>
+          </div>
+          <div className="col-6">
+            <input className="form-control"
+              type="text"
+              readOnly={true}
+              value={window.location}
+              id="url"/>
+          </div>
+          <div className="col-1">
+            <button className="btn btn-link" onClick={this.props.resetChoices}>
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     );
+  }
+
+  copyLink() {
+    const url = document.getElementById("url");
+    url.select();
+    document.execCommand("copy");
   }
 }
 
