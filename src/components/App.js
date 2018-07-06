082a339685f0ec544ce8de2e96657d30f2105dba
Michael Borromeo <mborromeo@gmail.com>
2018-07-06 15:53:59 -0400

    Move summary to top

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,9 @@ class App extends Component {
     return (
       <div className="App container-fluid">
         <Header/>
+        <Summary/>
         <Choices/>
         <Types/>
-        <Summary/>
       </div>
     );
   }
